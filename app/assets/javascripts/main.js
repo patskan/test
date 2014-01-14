0139765f35fc3ffa7caad16c8a15a573aebc5a50
Valentyn <valik.patskan@gmail.com>
2014-01-14 21:59:41 +0200

    Fixed issue when content was dragged too far on left and right.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var right = $(".right");
 	var box = $(".big.box");
 	var slider = $(".casket");
+	var margin = 20;
 	var sliderWidth = 0;
 	var complete = true;
 
@@ -16,18 +17,32 @@ $(document).ready(function(){
 	$(".right").on("click", function(){
 		if( (sliderWidth + slider.position().left) > box.width() && complete){
 			complete = false;
-			slider.stop().animate({
-				left: slider.position().left - box.width() - 20
-			}, 400, function(){ complete = true; });
+			if( (slider.position().left - box.width() - margin + sliderWidth) >= box.width() ){
+				slider.stop().animate({
+					left: slider.position().left - box.width() - margin
+				}, 400, function(){ complete = true; });
+			}
+			else{
+				slider.stop().animate({
+					left: slider.position().left - (sliderWidth + slider.position().left - box.width())
+				}, 400, function(){ complete = true; });
+			}
 		}
 	});
 
 	$(".left").on("click", function(){
 		if( slider.position().left < 0 && complete){
 			complete = false;
-			slider.stop().animate({
-				left: slider.position().left + box.width() + 20
-			}, 400, function(){ complete = true; });
+			if((slider.position().left + box.width() + margin) <= 0){
+				slider.stop().animate({
+					left: slider.position().left + box.width() + margin
+				}, 400, function(){ complete = true; });
+			}
+			else{
+				slider.stop().animate({
+					left: 0
+				}, 400, function(){ complete = true; });
+			}
 		}
 	});
 
