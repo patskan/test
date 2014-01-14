92941efa97d1e3c5721d7873939664b0c8c6fab3
Valentyn <valik.patskan@gmail.com>
2014-01-14 16:42:15 +0200

    Fixes.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,22 +5,29 @@ $(document).ready(function(){
 	var box = $(".big.box");
 	var slider = $(".casket");
 	var sliderWidth = 0;
+	var complete = true;
 
 	slider.children().each(function(){
-		sliderWidth = sliderWidth + $(this).width();
+		sliderWidth = sliderWidth + $(this).outerWidth(true);
 	});
 
 	slider.css("width", sliderWidth);
 
 	$(".right").on("click", function(){
-		if( (sliderWidth + slider.position().left) > box.width() ){
-			slider.animate({ left: slider.position().left - box.width() - 20 }, 500 );
+		if( (sliderWidth + slider.position().left) > box.width() && complete){
+			complete = false;
+			slider.stop().animate({
+				left: slider.position().left - box.width() - 20
+			}, 400, function(){ complete = true; });
 		}
 	});
 
 	$(".left").on("click", function(){
-		if( slider.position().left != 0 ){
-			slider.animate({ left: slider.position().left + box.width() + 20 }, 500 );
+		if( slider.position().left < 0 && complete){
+			complete = false;
+			slider.stop().animate({
+				left: slider.position().left + box.width() + 20
+			}, 400, function(){ complete = true; });
 		}
 	});
 
